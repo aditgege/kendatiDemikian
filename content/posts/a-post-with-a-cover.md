c09ed8b66191a577925f323b4b0c39ef03134cbc
aditgege <aditgege0@gmail.com>
2019-03-24 01:35:20 +0700

    error coverimg

---
+++ content/posts/a-post-with-a-cover.md
@@ -0,0 +1,13 @@
+---
+title: A post with a cover image
+date: 2019-01-07
+published: true
+tags: ['Markdown', 'Cover Image']
+series: false
+cover_image: ./images/alexandr-podvalny-220262-unsplash.jpg
+canonical_url: false
+description: "Test Gambar"
+---
+test Gambar
+
+
--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -7,10 +7,6 @@
     <div class="posts">
       <PostCard v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" :post="edge.node"/>
     </div>
-    <!--  
-            coverImage (width: 860, blur: 10)
-
-    -->
   </Layout>
 </template>
 
@@ -27,6 +23,7 @@ query Tag ($id: String!) {
             date (format: "D. MMMM YYYY")
             timeToRead
             description
+            coverImage (width: 860, blur: 10)
             content
           }
         }
